a7ed3daeac66cd29d157c829f4ffb74d3259ed95
Clint Madariaga <clintmad21@hotmail.com>
2016-10-27 10:04:15 -0600

    Yup

--- public/planets/app.js
+++ public/planets/app.js
@@ -1,22 +1,22 @@
-// ;(function(){
+;(function(){
 
-//   angular.module('da-planets', [])
-//     .component('universe', {
-//       template: `hello <button ng-click="$ctrl.loadStuff()">click me please</button>
-//         <div ng-repeat="g in $ctrl.galaxies">
-//           <p>{{g.name}}</p>
-//         </div>
-//       `,
-//       controller: UniverseController
-//     })
+  angular.module('da-planets', [])
+    .component('universe', {
+      template: `hello <button ng-click="$ctrl.loadStuff()">click me please</button>
+        <div ng-repeat="g in $ctrl.galaxies">
+          <p>{{g.name}}</p>
+        </div>
+      `,
+      controller: UniverseController
+    })
 
-//     function UniverseController($http) {
-//       let $ctrl = this;
-//       this.loadStuff = function() {
-//         $http.get('/api/galaxies').then(function(res){
-//           $ctrl.galaxies = res.data
-//         })
-//       }
-//     }
+    function UniverseController($http) {
+      let $ctrl = this;
+      this.loadStuff = function() {
+        $http.get('/api/galaxies').then(function(res){
+          $ctrl.galaxies = res.data
+        })
+      }
+    }
 
-// }());
+}());
